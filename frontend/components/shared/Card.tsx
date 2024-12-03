7f5a9166a23592fbdd065f59cb3df424421bc9bd
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2024-12-03 22:05:32 +0700

    Changes in design of Card,Modal,Footer

--- frontend/components/shared/Card.tsx
+++ frontend/components/shared/Card.tsx
@@ -9,25 +9,56 @@ export interface CardProp extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export const Card = ({ item, className, ...props }: CardProp) => {
+  const color =
+    item.status == 'Closed'
+      ? 'border-red-300 text-red-500 bg-red-100'
+      : 'border-green-300 text-green-500 bg-green-100';
+  const designStatus = `rounded-2xl border-2 px-2 text-sm ${color}`;
+
   return (
     <div
       className={cn(
-        'flex items-center gap-5 rounded-3xl bg-white px-6 py-3',
+        'flex flex-col items-center gap-3 bg-white pb-3 shadow-lg duration-100 hover:scale-105',
         className,
       )}
       {...props}
     >
-      <Image
-        src={item.img}
-        width="100"
-        height="100"
-        alt={item.desc}
-        className="rounded-full"
-      />
-      <div>
-        <h4 className="text-lg font-semibold">{item.title}</h4>
+      <div className="relative h-60 w-full shrink-0">
+        <Image
+          src={item.img}
+          fill
+          alt={item.desc}
+          className="object-cover shadow-lg"
+        />
+      </div>
+      <div className="flex max-w-[80%] flex-col justify-center gap-2">
+        <div>
+          <span className={designStatus}>{item.status}</span>
+          <h4 className="text-md font-semibold">{item.title}</h4>
+        </div>
         <p>{item.desc}</p>
-        <p>{item.upvotes}</p>
+        <div className="flex w-full items-center justify-evenly pt-10">
+          <div className="flex items-center gap-1">
+            <Image
+              src="https://www.svgrepo.com/show/509193/people.svg"
+              width={13}
+              height={13}
+              alt={item.desc}
+              className="size-4"
+            />
+            <p className="text-sm">{item.upvotes.length}</p>
+          </div>
+          <div className="flex items-center gap-1">
+            <Image
+              src="https://www.svgrepo.com/show/501544/email.svg"
+              width={13}
+              height={13}
+              alt={item.desc}
+              className="size-4"
+            />
+            <p className="text-sm">{item.submissionDate}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/components/shared/Modal.tsx
+++ frontend/components/shared/Modal.tsx
@@ -1,22 +1,64 @@
-import Link from 'next/link';
+'use client';
+import Image from 'next/image';
+import { Button } from './Button';
 
-export default function Modal() {
+type ModalProps = {
+  closeModal: () => void;
+};
+
+export default function Modal({ closeModal }: ModalProps) {
   return (
     <div className="fixed inset-0 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50">
-      <div className="w-96 rounded-md border bg-white p-8 shadow-lg">
-        <div className="text-center">
-          <h3 className="text-2xl font-bold text-gray-900">Modal Title</h3>
-          <div className="mt-2 px-7 py-3">
-            <p className="text-lg text-gray-500">Modal Body</p>
-          </div>
-          <div className="mt-4 flex justify-center">
-            <Link
-              href="/profilepage"
-              className="rounded-md bg-blue-500 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+      <div className="h-96 w-[60%] rounded-lg border bg-white p-8 shadow-lg">
+        <div>
+          <div className="flex justify-between px-5">
+            <h3 className="text-lg font-bold text-gray-900">Change Profile</h3>
+            <button
+              onClick={closeModal}
+              className="rounded-full px-4 py-2 duration-700 hover:bg-red-400 focus:outline-none"
             >
-              Close
-            </Link>
+              <Image
+                src="https://www.svgrepo.com/show/522506/close.svg"
+                width={13}
+                height={13}
+                alt="close button"
+                className="size-4"
+              />
+            </button>
           </div>
+          <form className="mt-2 px-7 py-3">
+            <div className="flex flex-col">
+              <div className="mb-5">
+                <label
+                  htmlFor="name"
+                  className="text-md mb-2 block font-medium text-gray-900"
+                >
+                  Insert name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="block w-full rounded-lg border border-gray-400 bg-slate-200 p-2 text-sm text-gray-900 focus:outline-none"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="bio"
+                  className="text-md mb-2 block font-medium text-gray-900"
+                >
+                  Change bio
+                </label>
+                <textarea
+                  id="bio"
+                  className="block w-full resize-none rounded-lg border border-gray-400 bg-slate-200 p-2 text-sm text-gray-900 focus:outline-none"
+                  maxLength={80}
+                />
+              </div>
+            </div>
+            <Button className="right-0 mt-5 border-green-200 bg-green-500 text-white hover:border-green-200 hover:bg-white hover:text-black">
+              Change
+            </Button>
+          </form>
         </div>
       </div>
     </div>
