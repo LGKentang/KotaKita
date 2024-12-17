3a9de9019490987ae6e3c7b339905da8b7c8137a
Lewi Meyvianus Borosi <lewiyeeey@gmail.com>
2024-12-17 21:16:09 +0700

    Error handling parse.ts

--- frontend/libs/utils/parse.ts
+++ frontend/libs/utils/parse.ts
@@ -1,8 +1,14 @@
 export default function parseImageUrl(imageUrl: string) {
-    if (imageUrl.startsWith("http") || imageUrl.startsWith("https")) {
+  try {
+    if (!imageUrl) {
+      throw new Error('imageUrl is not defined');
+    }
+    if (imageUrl.startsWith('http') || imageUrl.startsWith('https')) {
       return imageUrl;
     } else {
       return `${process.env.NEXT_PUBLIC_BACKEND_URI_IMAGE}${imageUrl}`;
     }
+  } catch (error) {
+    return error;
+  }
 }
-  
